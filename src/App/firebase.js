8c85e483ca2520c904f908563e9aabf9b905349a
SandroMoretti <jniorsandro@gmail.com>
2021-04-29 00:49:47 -0300

    arquivo de template para config do firebase; readme atualizado

--- src/App/firebase.js
+++ src/App/firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyANgF3VQW5m2zQor9yJjXKXPGBn05965W4",
-  authDomain: "fir-auth-example-59904.firebaseapp.com",
-  projectId: "fir-auth-example-59904",
-  storageBucket: "fir-auth-example-59904.appspot.com",
-  messagingSenderId: "211136169494",
-  appId: "1:211136169494:web:ceedfaa5d3279b62911737",
-  measurementId: "G-64WQDQTNL6"
-};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
